346f90c8fc7423d8b3b981e4e9147c57bde9b5dc
andrea <andrea.gasparin.87@gmail.com>
2026-01-02 14:34:03 +0100

    added fast knapsack

--- GA_CPP/GA/genetic_cpp.py
+++ GA_CPP/GA/genetic_cpp.py
@@ -8,7 +8,8 @@ from numpy.ctypeslib import ndpointer
 
 
 class GeneticCpp:
-    def __init__(self, upper_bounds, lower_bounds, commodities_tax_free: np.array, n_users: np.array, transfer_costs: np.array,
+    def __init__(self, upper_bounds, lower_bounds, commodities_tax_free: np.array, n_users: np.array,
+                 transfer_costs: np.array,
                  n_commodities, n_paths,
                  pop_size, offs_size, mutation_rate, recombination_size,
                  verbose, n_threads=None, seed=None):
@@ -20,12 +21,12 @@ class GeneticCpp:
         self.lib = ctypes.CDLL('GA_CPP/GA/ga_bridge.so')
 
         self.lib.Genetic_.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double),
-                                         ctypes.POINTER(ctypes.c_double),
-                                         ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_double),
-                                         ctypes.c_short, ctypes.c_short,
-                                         ctypes.c_short, ctypes.c_short, ctypes.c_double,
-                                         ctypes.c_short,
-                                         ctypes.c_short, ctypes.c_bool, ctypes.c_short]
+                                      ctypes.POINTER(ctypes.c_double),
+                                      ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_double),
+                                      ctypes.c_short, ctypes.c_short,
+                                      ctypes.c_short, ctypes.c_short, ctypes.c_double,
+                                      ctypes.c_short,
+                                      ctypes.c_short, ctypes.c_bool, ctypes.c_short]
         self.lib.Genetic_.restype = ctypes.c_void_p
 
         self.lib.destroy.argtypes = [ctypes.c_void_p]
@@ -41,17 +42,20 @@ class GeneticCpp:
 
         self.lib.run_genetic_.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_double), ctypes.c_int]
 
+        self.lib.eval_.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_double)]
+        self.lib.eval_.restype = ndpointer(dtype=ctypes.c_double, shape=(pop_size,))
+
         n_usr = np.array(n_users, dtype=np.int32)
 
         self.genetic = self.lib.Genetic_(upper_bounds.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                                            lower_bounds.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                                            commodities_tax_free.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                                            n_usr.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
-                                            transfer_costs.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                                            ctypes.c_short(n_commodities), ctypes.c_short(n_paths),
-                                            ctypes.c_short(pop_size), ctypes.c_short(offs_size),
-                                            ctypes.c_double(mutation_rate), ctypes.c_short(recombination_size),
-                                            ctypes.c_short(num_threads), ctypes.c_bool(verbose), ctypes.c_short(seed))
+                                         lower_bounds.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                                         commodities_tax_free.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                                         n_usr.ctypes.data_as(ctypes.POINTER(ctypes.c_int)),
+                                         transfer_costs.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                                         ctypes.c_short(n_commodities), ctypes.c_short(n_paths),
+                                         ctypes.c_short(pop_size), ctypes.c_short(offs_size),
+                                         ctypes.c_double(mutation_rate), ctypes.c_short(recombination_size),
+                                         ctypes.c_short(num_threads), ctypes.c_bool(verbose), ctypes.c_short(seed))
 
     def __del__(self):
         self.lib.destroy(ctypes.c_void_p(self.genetic))
@@ -61,6 +65,10 @@ class GeneticCpp:
                               population.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), ctypes.c_int(iterations))
 
         return self.lib.get_gen_best_val_(ctypes.c_void_p(self.genetic))
+
+    def eval(self, population):
+        return self.lib.eval_(ctypes.c_void_p(self.genetic),
+                              population.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
 
     def get_results(self):
         a = self.lib.get_population_(ctypes.c_void_p(self.genetic))
--- Solver/genetic_solver.py
+++ Solver/genetic_solver.py
@@ -59,3 +59,7 @@ class Genetic:
                 np.random.choice(vals, size=self.pop_size, replace=True))
         return population
         # self.vals = np.array([self.fitness_fun(sol) for sol in self.population])
+
+    def eval(self, init_population):
+        self.final_vals = self.genetic.eval(init_population)
+        self.best_val = self.final_vals.max()
