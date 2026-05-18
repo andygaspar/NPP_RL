fixed issues of computing max_w and lower level computation with p adjusted

KP_Solver/knap_cpp.py
@@ -20,7 +20,10 @@ class KnapCpp:
     def __init__(self, pb, batch_size):
         self.lib = ctypes.CDLL('KP_Solver/knap_bridge.so')
         self.batch_size = batch_size
-        self.p = np.ascontiguousarray(np.array([pb.p for _ in range(self.batch_size)], dtype=float))
+
+        adjusted_p = pb.p + pb.adjustment_p
+
+        self.p = np.ascontiguousarray(np.array([adjusted_p for _ in range(self.batch_size)], dtype=float))
         self.c = np.ascontiguousarray(np.stack((pb.c,) * batch_size))
         self.w = np.ascontiguousarray(np.array([pb.w for _ in range(self.batch_size)]))
         # p = np.ascontiguousarray(p, dtype=np.float64)

SKWP/skwp_instance.py
@@ -13,14 +13,22 @@ class SKWP_instance:
         self.w = np.random.uniform(1, 100, size=(self.K, self.M))
         # self.c = self.w.sum(axis=1)//4
         # self.p = self.w + 10
-        self.c = np.random.uniform(self.w.sum(axis=1)*0.1, self.w.sum(axis=1)*0.9)
+        self.c = np.random.uniform(self.w.sum(axis=1) * 0.1, self.w.sum(axis=1) * 0.9)
         self.p = np.random.uniform(1, 100, size=(self.K, self.M))
 
+        self.p = np.round(self.p, 9)
+        self.c = np.round(self.c, 9)
+        self.w = np.round(self.w, 9)
+
+        idx_p = np.argsort(-self.p[:, : self.L], axis=-1)
+        adj_p = np.argsort(idx_p, axis=-1)
+        self.adjustment_p = np.zeros_like(self.p)
+        self.adjustment_p[:, :self.L] += (adj_p + 1) * 1e-16  # self.max_w =  self.w[:, :self.L].max(axis=0)
+
         # self.max_w =  self.w[:, :self.L] .max(axis=0)
-        self.max_w_ = self.compute_max() * 1.01
-        self.max_w = self.c
+        self.max_w = self.compute_max()
+        # self.max_w = self.c
         self.max_e_inv = (self.p[:, self.L:] / self.w[:, self.L:]).max(axis=1)
-        pass
 
     def compute_max(self):
         p = self.p[:, self.L:]
@@ -32,47 +40,41 @@ class SKWP_instance:
         cs = np.cumsum(w_sorted, axis=-1)
         sol = (cs < self.c[:, np.newaxis])
 
-        if sol.sum() == 0:
-            min_efficiency = self.p[:, :self.L].min() / self.c.max()
-        else:
-            min_efficiency = efficiency_sorted[sol].min()
-        max_w = self.p[:, :self.L].max(axis=0) / min_efficiency
+        min_cap_left = (self.c - (w_sorted * sol).sum(axis=-1)).min()
 
-        # for i in range(self.L):
-        #     max_w[i] = self.p[:, i].max() / min_efficiency
-            # for k in range(self.K):
-            #     for j in range(1, sol.shape[1]):
-            #         if self.p[k, i] / efficiency_sorted[k, j] + cs[k, j - 1] <= self.c[k]:
-            #             max_w[i] = self.p[k, i] / efficiency_sorted[k, j]
+        min_efficiency = efficiency_sorted[sol].min()
+        max_w = self.p[:, :self.L].max(axis=0) / min_efficiency
+        max_w[max_w < min_cap_left] = min_cap_left
+        max_w[max_w > self.c.max()] = self.c.max()
 
         return max_w
 
-    def compute_obj(self, w):
-        w = np.concat([w, self.w[:, self.L:]], axis=-1)
-        efficiency = self.p / w
-        indexes = np.argsort(-efficiency, axis=-1)
-        w_sorted = np.take_along_axis(w, indexes, axis=-1)
-        cs = np.cumsum(w_sorted, axis=-1)
-        sol = (cs < self.c[:, np.newaxis])
-        reversed_indexes = np.argsort(indexes, axis=-1)  # This gives you the reverse mapping
-        # sol_original_order = np.take_along_axis(sol, reversed_indexes, axis=-1)
-        # new_sol = np.zeros_like(sol)
-        np.put_along_axis(sol, indexes, sol, axis=-1)
-        return (w*sol)[:self.L].sum(axis=-1)
-
-    def compute_obj_2(self, w_new_):
+    def compute_obj(self, w_new_):
         w_new = np.stack((w_new_,) * self.K, axis=1)
         w = self.w.copy()
         w[:, :self.L] = w_new
-        efficiency = self.p / w
-        identical = np.any(np.diff(efficiency, axis=-1) == 0, axis=-1)
-        indexes = np.argsort(-efficiency, axis=-1)
+        inv_efficiency = np.round(w / (self.p + self.adjustment_p), 9)
+        # identical = np.any(np.diff(efficiency, axis=-1) == 0, axis=-1)
+        indexes = np.argsort(inv_efficiency, axis=-1)
         w_sorted = np.take_along_axis(w, indexes, axis=-1)
         cs = np.cumsum(w_sorted, axis=-1)
-        sol = (cs < self.c[ :, np.newaxis])
+        sol = (cs < self.c[:, np.newaxis])
         np.put_along_axis(sol, indexes, sol, axis=-1)
-        vals__ = (w * sol)[:, :self.L].sum(axis=-1).sum(axis=-1)
-        return vals__, sol
+        val = (w * sol)[:, :self.L].sum(axis=-1).sum(axis=-1)
+        return val, sol
+
+    # def compute_obj(self, w):
+    #     w = np.concat([w, self.w[:, self.L:]], axis=-1)
+    #     efficiency = self.p / w
+    #     indexes = np.argsort(-efficiency, axis=-1)
+    #     w_sorted = np.take_along_axis(w, indexes, axis=-1)
+    #     cs = np.cumsum(w_sorted, axis=-1)
+    #     sol = (cs < self.c[:, np.newaxis])
+    #     reversed_indexes = np.argsort(indexes, axis=-1)  # This gives you the reverse mapping
+    #     # sol_original_order = np.take_along_axis(sol, reversed_indexes, axis=-1)
+    #     # new_sol = np.zeros_like(sol)
+    #     np.put_along_axis(sol, indexes, sol, axis=-1)
+    #     return (w*sol)[:self.L].sum(axis=-1)
 
     # t = time.time()
     # efficiency = self.p / (w + 1e-12)
@@ -85,10 +87,9 @@ class SKWP_instance:
     # print(time.time() - t, 'np')
     # t = time.time()
 
-
     def sort_values(self):
         for k in range(self.K):
-            idx = np.argsort(self.p[k]/self.w[k])[::-1]
+            idx = np.argsort(self.p[k] / self.w[k])[::-1]
             self.p[k] = self.p[k][idx]
             self.w[k] = self.w[k][idx]
 
@@ -97,7 +98,3 @@ class SKWP_instance:
 
     def __str__(self):
         return 'M:' + str(self.M) + '-K:' + str(self.K)
-
-
-
-